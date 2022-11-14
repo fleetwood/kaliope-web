cd0a11e7eac67e98e221ccb16504bcf2a4c960bc
Fleetwood <wizening@gmail.com>
2022-11-13 22:43:24 -0600

    Querying graphql into MainContent > FeedItem

--- components/containers/feedItem.tsx
+++ components/containers/feedItem.tsx
@@ -1,44 +1,42 @@
-import { ReactNode } from "react";
+import React from "react";
+import { Post, PostAuthor } from "../../graphql/post/query";
 
-type FeedItemProps = {
-  image?: string;
-  author: string;
-  description?: string;
-  content: ReactNode;
-};
-
-const FeedItem = ({
-  image,
-  author,
-  description,
-  content,
-}: FeedItemProps) => {
-  return (
-    <div className="relative col-span-2 rounded-3xl border border-gray-100 p-4 mb-4 shadow-2xl shadow-cyan-900/10 after:absolute after:inset-0 after:right-8 after:ml-auto after:mt-auto after:h-px after:w-2/3 after:bg-gradient-to-l after:from-transparent after:via-cyan-300 after:to-transparent dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:after:via-gray-400">
-      <div className="flex items-center gap-4">
-        {image && (
-          <img
-            src={image}
-            loading="lazy"
-            width="412"
-            height="412"
-            className="h-16 w-16 rounded-xl"
-          />
-        )}
-        <div>
+class FeedItem extends React.Component<Post> {
+  render() {
+    const { title, subtitle, content, author } = this.props;
+    return (
+      <div className="relative col-span-2 rounded-3xl border border-gray-100 p-4 mb-4 shadow-2xl shadow-cyan-900/10 after:absolute after:inset-0 after:right-8 after:ml-auto after:mt-auto after:h-px after:w-2/3 after:bg-gradient-to-l after:from-transparent after:via-cyan-300 after:to-transparent dark:border-gray-700 dark:bg-gray-800 dark:shadow-none dark:after:via-gray-400">
+        <div className="flex items-center gap-4">
+          {author?.photoURL && (
+        <img
+          src={author.photoURL}
+          width="412"
+          height="412"
+          className="h-16 w-16 rounded-xl"
+          loading="lazy"
+        />
+      )}
+          <div>
+            {author?.displayName && (
           <h3 className="text-xl font-semibold text-gray-800 dark:text-white">
-            {author}
+            {author.displayName}
           </h3>
-          {description && (
+        )}
             <span className="text-sm text-gray-500 dark:text-gray-300">
-              {description}
+              {title}
             </span>
-          )}
+
+            {subtitle && (
+              <span className="text-sm text-gray-500 dark:text-gray-300">
+                {subtitle}
+              </span>
+            )}
+          </div>
         </div>
+        <p className="mb-6 mt-4 text-gray-600 dark:text-gray-200">{content}</p>
       </div>
-      <p className="mb-6 mt-4 text-gray-600 dark:text-gray-200">{content}</p>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default FeedItem;
--- components/containers/mainContent.tsx
+++ components/containers/mainContent.tsx
@@ -1,19 +1,36 @@
-import { ReactNode } from "react";
+import { useEffect, useState } from "react";
+import Spinner from "../ui/spinner";
+
+import { getMainPosts, Post } from "./../../graphql/post/query";
 import FeedItem from "./feedItem";
 
-type MainContentProps = {
-  className?: string;
-  children?: ReactNode;
-};
+const MainContent = () => {
+  const [posts, setPosts] = useState<Array<Post>>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-const MainContent = ({ className, children }: MainContentProps) => (
-  <div className={className}>
-    <div className="mx-auto max-w-5xl px-8 lg:px-0">
-      <FeedItem author="Bob" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum corrupti vel odit natus iure nostrum, eveniet ipsum ad, cumque voluptatem tempore odio explicabo rem illo temporibus quibusdam dolorem neque ex."/>
-      <FeedItem author="Bob" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum corrupti vel odit natus iure nostrum, eveniet ipsum ad, cumque voluptatem tempore odio explicabo rem illo temporibus quibusdam dolorem neque ex."/>
-      <FeedItem author="Bob" content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum corrupti vel odit natus iure nostrum, eveniet ipsum ad, cumque voluptatem tempore odio explicabo rem illo temporibus quibusdam dolorem neque ex."/>
-    </div>
-  </div>
-);
+  const fetchData = async () =>
+    getMainPosts()
+      .then((results:Array<Post>) => {
+        if (results) {
+          setPosts(results.map(p => new Post(p)))
+          setIsLoading(false);
+        }
+      })
+      .catch((e) => {
+        console.error("fetchData FAIL", e);
+        //set an error here....
+      });
+  fetchData();
+
+  if (isLoading) return <Spinner height={4} width={4} />;
+
+  return (
+      <div className="mx-auto max-w-5xl px-8 lg:px-0">
+        {posts.map((post) => (
+          <FeedItem title={post.title} content={post.content} author={post.author} />
+          ))}
+      </div>
+  );
+};
 
 export default MainContent;
